9657846842e5346e5883bc5d33d05dcc4d4e1735
jim.chen <azx011014@gmail.com>
2023-11-18 00:34:26 +0800

    [feature]: add Navbar and Footer components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import AuthProviders from "./AuthProviders";
+import { NavLinks } from "@/constants";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const session = null;
+
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
+        </Link>
+        <ul className="hidden text-small gap-7 xl:flex">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Woek</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
